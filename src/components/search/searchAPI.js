bffd3cc30da3ce76e432ba03767e7664173f4400
Kozyrov <sfrankie11@gmail.com>
2021-10-17 12:52:56 -0700

    basics of http call to oni api

---
+++ src/components/search/searchAPI.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const searchByTitle = async (term) => {
+    try {
+        await axios.post("https://oni-search-api-apim.azure-api.net/oni-search-api", {
+            "value": term
+        }).then((response) => {
+            console.log(`success: ${response}`);
+        }).catch((err) => {
+            console.log(`error: ${err}`);
+        })
+    } catch (err) {
+        throw new Error("the call failed");
+    }
+}
