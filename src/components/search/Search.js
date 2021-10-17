61507631398e8c0df2a19d3c3d8bca28f8fab6a0
Kozyrov <sfrankie11@gmail.com>
2021-10-17 12:42:09 -0700

    removing client auth code. adding in search component

---
+++ src/components/search/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Search = () => {
+    return (
+        <div className="search-field">
+            <input type="text" placeholder="What Anime are you looking for?"></input>
+        </div>
+    );
+}
+
+export default Search;
