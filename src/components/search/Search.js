10f3341b328dd0eaef91d8291fb439bc7db4f444
Kozyrov <sfrankie11@gmail.com>
2021-10-17 17:21:17 -0700

    api post search pipeline setup complete

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { searchByTitle } from "./searchAPI";
 
 const Search = () => {
+    const [value, setValue] = useState("");
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        await searchByTitle(value);
+    }
+
+    const handleInput = (event) => {
+        setValue(event.currentTarget.value);
+    }
+
     return (
-        <div className="search-field">
-            <input type="text" placeholder="What Anime are you looking for?"></input>
-        </div>
+        <form className="search-field" onSubmit={handleSubmit}>
+            <label>Search for anime by title:</label>
+            <input 
+                type="search"
+                id="title-search-input"
+                placeholder="What Anime are you looking for?"
+                aria-label="Search for anime based on title"
+                value={value}
+                onInput={handleInput}
+            ></input>
+        </form>
     );
 }
 
--- src/components/search/searchAPI.js
+++ src/components/search/searchAPI.js
@@ -2,9 +2,8 @@ import axios from "axios";
 
 export const searchByTitle = async (term) => {
     try {
-        await axios.post("https://oni-search-api-apim.azure-api.net/oni-search-api", {
-            "value": term
-        }).then((response) => {
+        await axios.post(`https://oni-search-api-apim.azure-api.net/oni-search-api/search?value=${term}`)
+        .then((response) => {
             console.log(`success: ${response}`);
         }).catch((err) => {
             console.log(`error: ${err}`);
