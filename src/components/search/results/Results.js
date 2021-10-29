62d60423b3c619ca068447a50dba6e3be5449217
Kozyrov <sfrankie11@gmail.com>
2021-10-28 20:01:16 -0700

    WIP creating results component

--- src/components/search/results/Results.js
+++ src/components/search/results/Results.js
@@ -1,9 +1,23 @@
 import React from "react"
 
-const Results = () => {
+const Results = (props) => {
+  const {searchResults} = props;
+
+  const resultsList = searchResults.map((item) => {
+    return (
+      <li>
+        <div>
+          {item.attributes.canonicalTitle}
+          <img src={item.attributes.posterImage.tiny} alt=""></img>
+        </div>
+      </li>
+    )
+  })
+
   return (
     <div>
       search results placeholder
+      {resultsList}
     </div>
   )
 }
